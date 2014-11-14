20bd9ea56edae54622a9f82563ddb2cfbe2e7b9c
unknown <gabeilko@gmail.com>
2014-11-13 21:04:04 -0500

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('farseerApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
