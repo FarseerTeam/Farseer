33068711a0b55d03ea004c899afaf911e6843093
Pedro Ribeiro <pedroribeirojr@gmail.com>
2015-02-13 05:41:41 -0500

    Using Promises in Players

--- server/components/players.spec.js
+++ server/components/players.spec.js
@@ -10,63 +10,77 @@ var dataService = require('./dataService')
 dataService.connect();
 
 
-describe("Players", function() {
-	//holds a players to use in the each test
-	var currentCustomer = null;
-	beforeEach(function(done) {
+describe("In the components/players module,", function() {
+    //holds a players to use in the each test
+    var currentCustomer = null;
+    beforeEach(function(done) {
 
-		//add some test data
-		players.Player.create({
-			name: "John",
-			email: "test@test.com"
-		}, function(err, doc) {
-			currentCustomer = doc;
-			done();
-		});
-	});
-	it("can be retrieved by email", function(done) {
-		players.findByEmail(currentCustomer.email, function(doc) {
-			doc.email.should.equal(currentCustomer.email);
-			done();
-		}, function(e) {});
-	});
-	afterEach(function(done) {
-		players.Player.remove({}, function() {
-			done();
-		});
-	});
+        //add some test data
+        players.Player.create({
+            name: "John",
+            email: "test@test.com"
+        }).then(function(doc) {
+            currentCustomer = doc;
+            done();
+        }, function(err) {
+            done(err)
+        });
 
-	describe("has an optional", function() {
-		it("team reference", function(done) {
-			var aTeam = {
-				name: "Ford"
-			};
-			teams.Team.create(aTeam,
-				function(err, team) {
-					should(team._id).be.ok;
-					var thePlayer = {
-						name: "AJohn",
-						email: "Atest@test.com",
-						_team: team
-					};
+    });
 
-					players.Player.create(thePlayer, function(err, doc) {
-						should(err).not.be.ok;
-						should(team._id.equals(doc._team)).be.truthy;
-						done();
-					});
-				});
-		});
+    describe("A player ", function() {
+        it("has an optional team reference", function(done) {
+            var theTeam;
+
+            teams.Team.create({
+                name: "Ford"
+            }).then(function(team) {
+                should(team._id).be.ok;
+                theTeam = team;
+
+                var thePlayer = {
+                    name: "AJohn",
+                    email: "Atest@test.com",
+                    _team: team
+                };
+
+                return players.Player.create(thePlayer);
 
 
-		afterEach(function(done) {
-			players.Player.remove({}, function() {
-				teams.Team.remove({}, function() {
-					done();
-				})
-			});
-		});
+            }, function(err) {
+                done(err)
+            }).then(function(player) {
+                should(theTeam._id.equals(player._team)).be.truthy;
 
-	});
+                done();
+            }, function(err) {
+                done(err)
+            });
+
+
+
+        });
+
+
+        afterEach(function(done) {
+            players.Player.remove({}, function() {
+                teams.Team.remove({}, function() {
+                    done();
+                })
+            });
+        });
+
+    });
+    it("Players can be found by email", function(done) {
+        players.findByEmail(currentCustomer.email, function(doc) {
+            doc.email.should.equal(currentCustomer.email);
+            done();
+        }, function(e) {});
+    });
+    afterEach(function(done) {
+        players.Player.remove({}, function() {
+            done();
+        });
+    });
 
 });
