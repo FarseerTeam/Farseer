a3a6a0623a01cfab259c1600b68e12bd475e435d
Chris Atkins <chrisatkins55@gmail.com>
2015-04-04 18:06:58 -0400

    Adding a function in components/players that will find a player by any unique identifier (email or id)

--- server/components/players.js
+++ server/components/players.js
@@ -22,16 +22,44 @@ module.exports = (function() {
       email: email
     }, function(err, doc) {
       if (err) {
-        fail(err)
+        fail(err);
       } else {
         success(doc);
       }
     });
   }
 
+  var _findById = function(id, success, fail) {
+    _model.findById(id, function(err, doc) {
+      if(err) {
+        fail(err);
+      } else {
+        success(doc);
+      }
+    });
+  }
+
+  var _findByAnyUniqueIdentifier = function(uniqueIdentifier, success, fail) {
+
+    if(isObjectId(uniqueIdentifier)) {
+      _findById(uniqueIdentifier, success, fail);
+    }
+    else {
+      _findByEmail(uniqueIdentifier, success, fail);
+    }
+  }
+
+  var isObjectId = function(id) {
+    var strId = String(id);
+    return /^[0-9a-fA-F]{24}$/.test(strId); 
+    // http://stackoverflow.com/questions/11985228/mongodb-node-check-if-objectid-is-valid 
+    // https://github.com/mongodb/js-bson/blob/master/lib/bson/objectid.js
+  }
+
   return {
     Player: _model,
     schema: _playerSchema,
-    findByEmail: _findByEmail
+    findByEmail: _findByEmail,
+    findByAnyUniqueIdentifier: _findByAnyUniqueIdentifier
   }
 })();
--- server/components/players.spec.js
+++ server/components/players.spec.js
@@ -11,25 +11,11 @@ dataService.connect();
 
 
 describe("In the components/players module,", function() {
-    //holds a players to use in the each test
-    var currentCustomer = null;
-    beforeEach(function(done) {
-
-        //add some test data
-        players.Player.create({
-            name: "John",
-            email: "test@test.com"
-        }).then(function(doc) {
-            currentCustomer = doc;
-            done();
-        }, function(err) {
-            done(err)
-        });
-
-    });
 
     describe("A player ", function() {
+        
         it("has an optional team reference", function(done) {
+            
             var theTeam;
 
             teams.Team.create({
@@ -45,8 +31,6 @@ describe("In the components/players module,", function() {
                 };
 
                 return players.Player.create(thePlayer);
-
-
             }, function(err) {
                 done(err)
             }).then(function(player) {
@@ -57,10 +41,7 @@ describe("In the components/players module,", function() {
                 done(err)
             });
 
-
-
         });
-
 
         afterEach(function(done) {
             players.Player.remove({}, function() {
@@ -71,16 +52,163 @@ describe("In the components/players module,", function() {
         });
 
     });
-    it("Players can be found by email", function(done) {
-        players.findByEmail(currentCustomer.email, function(doc) {
-            doc.email.should.equal(currentCustomer.email);
-            done();
-        }, function(e) {});
-    });
-    afterEach(function(done) {
-        players.Player.remove({}, function() {
-            done();
+
+    
+    describe("In the findByEmail function... ", function(){
+
+        //holds a players to use in the each test
+        var currentCustomer = null;
+
+        beforeEach(function(done) {
+
+            //add some test data
+            players.Player.create({
+                name: "John",
+                email: "test@test.com"
+            }).then(function(doc) {
+                currentCustomer = doc;
+                done();
+            }, function(err) {
+                done(err)
+            });
         });
+
+        it("Players can be found by email", function(done) {
+            players.findByEmail(currentCustomer.email, function(doc) {
+                doc.email.should.equal(currentCustomer.email);
+                done();
+            }, function(e) {});
+        });
+
+        it("'null' is returned (with no error) if no player matches the email.", function(done) {
+            players.findByEmail('bad-email@noplace.com', function(doc) {
+                should.not.exist(doc);
+                done();
+            }, function(e) {
+                should.fail();
+            });
+        });
+
+        afterEach(function(done) {
+            players.Player.remove({}, function() {
+                done();
+            });
+        });
+
+    });
+
+    describe("In the findByAnyUniqueIdentifier function... ", function(){
+
+        var savedPlayer = null;
+
+        beforeEach(function(done) {
+            players.Player.create({
+                name: "John",
+                email: "test@tst.com"
+            }).then(function(doc) {
+                savedPlayer = doc;
+                done();
+            }, function(err) {
+                done(err)
+            });
+        });
+
+        it("Players can be found by email", function(done) {
+            players.findByAnyUniqueIdentifier(savedPlayer.email, function(doc) {
+                validatePlayer(doc, done);
+            }, function(e) {
+                console.log(e);
+                should.fail();
+            });
+        });
+
+        it("Players can be found by id", function(done) {
+            players.findByAnyUniqueIdentifier(savedPlayer._id, function(doc) {
+                validatePlayer(doc, done);
+            }, function(e) {
+                should.fail();
+            });
+        });
+
+        it("'null' is returned (with no error) if no player matches the passed value.", function(done) {
+            players.findByEmail('bad-value', function(doc) {
+                should.not.exist(doc);
+                done();
+            }, function(e) {
+                should.fail();
+            });
+        });
+
+        var validatePlayer = function(player, done) {
+            should.exist(player);
+            savedPlayer.name.should.equal(player.name);
+            savedPlayer.email.should.equal(player.email);
+            (savedPlayer._id.equals(player._id)).should.be.ok;
+            done();
+        }
+
+        afterEach(function(done) {
+            players.Player.remove({}, function() {
+                done();
+            });
+        });
+
+        describe("if there is a database error... ", function() {
+
+            var actualDatabaseFindOneFunction;
+            var actualDatabaseFindByIdFunction;
+            var expectedEmailError = 'findByEmailError';
+            var expectedIdError = 'findByIdError';
+            var validId = '552058b0469006560cad7c58';
+            var email = "hi@there.com";
+
+            beforeEach(function(done) {
+                mockTheDatabase_ToReturnAnError();
+                done();
+            });
+
+            afterEach(function(done) {
+                unmock();
+                done();
+            });
+
+            var mockTheDatabase_ToReturnAnError = function() {
+                actualDatabaseFindOneFunction = mongoose.Model.findOne;
+                actualDatabaseFindByIdFunction = mongoose.Model.findById;
+
+                mongoose.Model.findOne = function(modelObject, callback) {
+                    callback(expectedEmailError, undefined);
+                };
+                mongoose.Model.findById = function(id, callback) {
+                    callback(expectedIdError, undefined);
+                };
+            };
+
+            var unmock = function() {
+                mongoose.Model.findOne = actualDatabaseFindOneFunction;
+                mongoose.Model.findById = actualDatabaseFindByIdFunction;
+            }
+
+            it("the error function is called with the error returned from the database when finding by email.", function(done) {
+                players.findByAnyUniqueIdentifier(email, function(doc) {
+                    should.fail();
+                }, function(error) {
+                    expectedEmailError.should.be.equal(error);
+                    done();
+                });
+            });
+
+            it("the error function is called with the error returned from the database when finding by id.", function(done) {
+                players.findByAnyUniqueIdentifier(validId, function(doc) {
+                    should.fail();
+                }, function(error) {
+                    expectedIdError.should.be.equal(error);
+                    done();
+                });
+            });
+
+        });
+
     });
 
 });
