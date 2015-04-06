303688d26a104c2e4fc4043ddfe709762fbd0e84
Chris Atkins <chrisatkins55@gmail.com>
2015-04-06 00:00:56 -0400

    Done moving the functionality of assigning a player to a team into the appropriate endpoint (PUT to players/PLAYERID).  A lot of this functionality already existed, so I merged my changes into the existing update function.  We can now use player email as an id - and reference teams by team name.

--- server/api/player/index.js
+++ server/api/player/index.js
@@ -3,7 +3,6 @@
 var express = require('express');
 var controller = require('./players.controller');
 var middleware = require('../middleware');
-var players = require('../../components/players');
 var router = express.Router();
 
 router.route('/')
