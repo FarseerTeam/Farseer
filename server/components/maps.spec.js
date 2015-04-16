b2d9801749107dac170578ab5599d86d071b3d02
robertfmurdock <rmurdock@pillartechnology.com>
2015-04-16 14:47:14 -0400

    Brought most tests back to where Vera and I left off.

--- server/components/maps.spec.js
+++ server/components/maps.spec.js
@@ -52,6 +52,8 @@ describe('In the api/components/maps module,', function () { //jshint ignore:lin
       name: playerName,
       email: format("{}@test.smith.com", playerName),
       _team: path
+    }).then(function (playerMongoose) {
+      return playerMongoose.toObject();
     });
   };
   var doCreatePlayer = function (team, playerName) {
@@ -114,88 +116,42 @@ describe('In the api/components/maps module,', function () { //jshint ignore:lin
       }).then(function (players) {
         var expectedMap = {
           team: 'avatar',
-          players: [players.aang.toObject(), players.yung.toObject()]
+          players: [players.aang, players.yung]
         };
         return maps.buildTeamPlayersMap().then(checkMapMatches([expectedMap]));
       }).then(done, done);
     });
 
-    describe("Given player 'Aang' on team: 'avatar' and player 'Yung' on 'fireNation'", function () {
-      beforeEach(function (done) {
-        createTeam("avatar").then(
-          function (team) {
-            return createPlayer(team, "Aang");
-          }).then(createTeam("fireNation").then(
-            function (team) {
-              createPlayer(team, "Yung");
-              return null;
-            })).then(done, done);
-      });
-
-      var teamOne = {
-        team: 'avatar',
-        players: [{
-          name: 'Aang'
-        }]
-      };
-
-      var teamTwo = {
-        team: 'fireNation',
-        players: [{
-          name: 'Yung'
-        }]
-      };
-
-      it(shouldReturn([teamOne, teamTwo]), function (done) {
-
-
-        (function (expected, done) {
-          maps.buildTeamPlayersMap().then(checkMapMatches(expected)).then(done, done);
-        })([teamOne, teamTwo], done);
-
-      });
-
-
-      afterEach(function (done) {
-        clearAll(done);
-      });
+    it("Player 'Aang' on team: 'avatar' and player 'Yung' on 'fireNation' are separated in map", function (done) {
+      RSVP.hash({
+        aang: createPlayer('/avatar', "Aang"),
+        yung: createPlayer('/fireNation', "Yung")
+      }).then(function (players) {
+        var expectedMap = [{
+          team: 'avatar',
+          players: [players.aang]
+        }, {
+          team: 'fireNation',
+          players: [players.yung]
+        }];
+        return maps.buildTeamPlayersMap().then(checkMapMatches(expectedMap));
+      }).then(done, done);
     });
 
-    describe("Given player 'Aang' on team: 'avatar' under 'fireNation'", function () {
-
-      beforeEach(function (done) {
-
-        var p = createTeam("fireNation").then(function (team) {
-          return createTeam("avatar", team);
-        });
-        p = p.then(function (team) {
-          return createPlayer(team, 'Aang');
-        });
-        p = p.then(callDone(done), callDoneWithError(done));
-      });
-
-      var teamNode = {
-        team: 'fireNation',
-        players: [],
-        subTeams: [{
-          team: "avatar",
-          players: [{
-            name: "Aang"
-          }]
-        }]
-      };
-      it(shouldReturn(teamNode), function (done) {
-        (function (expected, done) {
-          maps.buildTeamPlayersMap().then(checkMapMatches(expected)).then(done, done);
-        })([teamNode], done);
-      });
-
-
-      afterEach(function (done) {
-        clearAll(done);
-      });
+    it("Given player 'Aang' on team: 'avatar' under 'fireNation'", function (done) {
+      createPlayer('/fireNation/avatar', "Aang")
+        .then(function (aang) {
+          var expectedMap = [{
+            team: 'fireNation',
+            players: [],
+            subTeams: [{
+              team: 'avatar',
+              players: [aang]
+            }]
+          }];
+          return maps.buildTeamPlayersMap().then(checkMapMatches(expectedMap));
+        }).then(done, done);
     });
-
 
     describe("Given players: [{name:'Zuko', team:'/fireNation/royalty'}, " + //
       "{name: 'Aang', team: '/avatar}, {name: 'Katara', team: '/avatar' }" + //
@@ -206,7 +162,6 @@ describe('In the api/components/maps module,', function () { //jshint ignore:lin
 
           var p = createTeam("avatar");
           p = p.then(function (team) {
-            // console.log(team);
             return createPlayer(team, 'Aang').then(
               doCreatePlayer(team, 'Katara'));
           });
