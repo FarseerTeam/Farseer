2f149d8a9534d3044a8b6792075f8720032514b2
Chris Atkins <chrisatkins55@gmail.com>
2015-05-18 23:30:20 -0400

    skeleton of playersMap image tests

---
+++ e2e/playersMap/player-images.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var page = require('./players-map.po');
+var setup = require('../common/data-setup');
+
+describe('The playersMap page has images for each player on it, and... ', function() {
+
+	it('the images are retrieved from gravatar.', function(done) {
+
+	});
+});
