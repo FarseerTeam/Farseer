d7349b68992cc834c96e95e760251a0564498b62
Chris Atkins <chrisatkins55@gmail.com>
2015-05-24 15:29:53 -0400

    a reasonable level of protractor tests for teams on the playersMap screen

--- e2e/playersMap/players-map.po.js
+++ e2e/playersMap/players-map.po.js
@@ -1,12 +1,36 @@
+/**
+ * This file uses the Page Object pattern to define the main page for tests
+ * https://docs.google.com/presentation/d/1B6manhG0zEXkC-H-tPo2vwU06JhL8w9-XCF9oehXzAQ
+ */
+
 'use strict';
 
 var PlayersMapPage = function() {
 
-	this.teams = element.all(by.css('li.team'));
-	this.players = element.all(by.css('li.player'));
+	var page = this;
+
+	this.allTeams = element.all(by.css('li.team'));
+
+	this.allPlayers = element.all(by.css('li.player'));
+	
 	this.teamNameFor = function(team) {
-		return team.element(by.css('p'));
-	}
+		return team.all(by.css('p')).get(0).getText();
+	};
+
+	this.team = function(teamName) {
+		return page.allTeams.filter(function(element, index) {
+			return page.teamNameFor(element).then(function(text) {
+				return text === teamName;
+			});
+		}).get(0);	
+	};
+
+	this.childTeamsOf = function(parentTeam) {
+		if (typeof parentTeam === 'string') {
+			return page.team(parentTeam).all(by.css('li.team'));
+		}
+		return parentTeam.all(by.css('li.team'));
+	};
 };
 
 module.exports = new PlayersMapPage();
--- e2e/playersMap/teams-in-map.spec.js
+++ e2e/playersMap/teams-in-map.spec.js
@@ -2,6 +2,7 @@
 
 var setup = require('../common/data-setup');
 var page = require('./players-map.po');
+var RSVP = require('rsvp');
 
 describe('The playersMap page has teams on it, and... ', function() {
 
@@ -15,7 +16,7 @@ describe('The playersMap page has teams on it, and... ', function() {
 			});
 
 			it('then there is no team displayed on screen.', function() {
-				expect(page.teams.count()).toBe(0);
+				expect(page.allTeams.count()).toBe(0);
 			});
 		});
 
@@ -31,25 +32,86 @@ describe('The playersMap page has teams on it, and... ', function() {
 				setup.purgeData().then(done);
 			});
 
-			it('there is a single team displayed on screen.', function(done) {
-				expect(page.teams.count()).toBe(1);
-				done();
+			it('there is a single team displayed on screen.', function() {
+				expect(page.allTeams.count()).toBe(1);
 			});
 
-			it('the team is assigned the name "unassigned"', function(done) {
-				expect(page.teamNameFor(page.teams.get(0)).getText()).toBe('unassigned');
-				done();
+			it('the team is assigned the name "unassigned"', function() {
+				expect(page.team('unassigned')).toBeDefined();
 			});
 		});
 	});
 
-	describe('when there are N teams defined with no parent teams... ', function() {
+	describe('there is a team with no players... ', function(){
+		
+		beforeEach(function(done) {
+			setup.addTeam('team1', '/team1')
+				.then(browser.get('playersMap'))
+				.then(done);
+		});
 
+		afterEach(function(done) {
+			setup.purgeData().then(done);
+		});
 
+		it('the team is not shown on screen', function() {
+			expect(page.allTeams.count()).toBe(0);
+		});
+	}); 
+
+	describe('when there are N teams defined with no parent teams (but with players)... ', function() {
+
+		beforeEach(function(done) {
+			setup.addTeam('team1', '/team1')
+				.then(setup.addTeam('team2', '/team2'))
+				.then(setup.addPlayer('a', 'amail', '/team1'))
+				.then(setup.addPlayer('b', 'bmail', '/team2'))
+				.then(browser.get('playersMap'))
+				.then(done);
+		});
+
+		afterEach(function(done) {
+			setup.purgeData().then(done);
+		});
+
+		it('every team is displayed on screen.', function(){
+			expect(page.allTeams.count()).toBe(2);
+		});
+
+		it('each team name is displayed on screen.', function(done){
+			RSVP.hash({
+				first: page.teamNameFor(page.allTeams.get(0)),
+				second: page.teamNameFor(page.allTeams.get(1))
+			}).then(function(teamNames){
+				expect(teamNames.first === 'team1' || teamNames.first == 'team2');
+				expect(teamNames.second === 'team1' || teamNames.second == 'team2');
+			}).then(done);
+		});
 	});
 
 	describe('when there is a team defined that has a parent team... ', function() {
+		
+		beforeEach(function(done) {
+			setup.addTeam('outerTeam', '/outerTeam')
+				.then(setup.addTeam('innerTeam', '/outerTeam/innerTeam'))
+				.then(setup.addPlayer('a', 'amail', '/outerTeam'))
+				.then(setup.addPlayer('b', 'bmail', '/outerTeam/innerTeam'))
+				.then(browser.get('playersMap'))
+				.then(done);
+		});
 
+		afterEach(function(done) {
+			setup.purgeData().then(done);
+		});
+
+		it('every team is displayed on screen.', function(){
+			expect(page.allTeams.count()).toBe(2);
+		});
+
+		it('the child team is held inside of the parent on screen.', function(){
+			expect(page.childTeamsOf('outerTeam').count()).toBe(1);
+			expect(page.teamNameFor(page.childTeamsOf('outerTeam').get(0))).toBe('innerTeam');
+		});
 
 	});
 });
