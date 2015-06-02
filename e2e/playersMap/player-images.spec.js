9a50d30ff6329761ab0cbfb5de9de4a19046a054
Chris Atkins <chrisatkins55@gmail.com>
2015-06-01 22:02:37 -0400

    A little bit of cleanup of the images e2e tests

--- e2e/playersMap/player-images.spec.js
+++ e2e/playersMap/player-images.spec.js
@@ -7,33 +7,30 @@ var RSVP = require('rsvp');
 describe('The playersMap page has images for each player on it, and... ', function() {
 
 	var imageTagsExist = function() {
-		return element(by.css('li.player img')).isPresent().then(function(val){
-			console.log("Images are present: " + val);
-			return val;
-		});
+		return element(by.css('li.player img')).isPresent();
     };
 
 	var allImagesAreLoaded = function() {
         return page.playerImages.then(function (images) {
-        	console.log('here with images' + images);
             return RSVP.hash({
             	one: images[0].getSize(),
             	two: images[1].getSize(),
             	three: images[2].getSize()
             }).then(function(results) {
-            	console.log('here with results: ' + results.one.height + '|' + results.two.height + '|' + results.three.height );
             	return results.one.height > 0 && results.two.height > 0 && results.three.height > 0;
             });
         }); 
 	};
 
-	beforeEach(function(done) {
+	beforeEach(function() {
 		this.addMatchers({
 	        toHaveHeightOfAtLeast: function (expectedSize) {
 	        	return this.actual.height >= expectedSize;
 	        }
 	    });
+	});
 
+	beforeEach(function(done) {
 		setup.addTeam('team1', '/team1')
 			.then(setup.addTeam('team2', '/team1/team2'))
 			.then(setup.addPlayer('p0', 'p0@somewhere.com'))
