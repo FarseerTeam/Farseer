d2b4b07175e9ea8a13b651f908f35dcf5e738b94
dimpar <dmitry.paremski@gmail.com>
2016-02-08 16:43:43 -0500

    changing the redirection from /playersMap to /worlds. Removing unnecessary test for main router

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -1,14 +1,13 @@
 'use strict';
 
 describe('The Home Page', function() {
-  var myElement;
 
   beforeEach(function() {
     browser.get('/');
   });
 
   it('redirects to playersMap', function(done) {
-    expect(browser.getCurrentUrl()).toContain('/playersMap');
+    expect(browser.getCurrentUrl()).toContain('/worlds');
     done();
   });
 });
