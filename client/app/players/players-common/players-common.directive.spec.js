1280299c78bd4678bf0f1ca55706bb8d16a6795d
dimpar <dmitry.paremski@gmail.com>
2016-02-15 17:30:53 -0500

    adding /worlds navigation directory

---
+++ client/app/players/players-common/players-common.directive.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: playersCommon', function () {
+
+  // load the directive's module and view
+  beforeEach(module('farseerApp'));
+  beforeEach(module('app/players/players-common/players-common.html'));
+
+  var element, scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<players-common></players-common>');
+    element = $compile(element)(scope);
+    scope.$apply();
+  }));
+});
