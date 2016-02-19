86d43b6f4cf7fd1f4b2abcd9a42d05024ab2a655
jgartee <garteejohn@johndeere.com>
2016-02-19 13:11:21 -0500

    add update world name test.

--- server/api/world/world.spec.js
+++ server/api/world/world.spec.js
@@ -65,7 +65,7 @@ describe('/api/worlds', function () {
         .expect('Content-Type', /json/)
         .end(function (err, res) {
         if (err) return done(err);
-        expect(res.body.name).to.be.eql('Lost World');
+        expect(res.body.name).to.be.eql(newWorld.name);
         done();
       });
     });
@@ -87,28 +87,40 @@ describe('/api/worlds', function () {
   });
 
   describe('PUT', function() {
+    var world = { name: 'Lost World' };
+    beforeEach(function(done) {
+
+      authenticatedRequest
+        .post('/api/worlds')
+        .send(world)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function (err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
     afterEach(function(done) {
       worlds.World.remove({}, function () {
         done();
       });
     });
 
-    //TODO: finish the tests
-    it('should update a world', function(done) {
-      var updatedWorld = {name: 'Updated World'};
+    it('should update a world name', function(done) {
+      var newWorldName = 'Updated World';
+      var request = {oldWorldName: world.name, updatedWorldName: newWorldName};
 
       authenticatedRequest
         .put('/api/worlds')
-        .send(updatedWorld)
+        .send(request)
         .expect(200)
         .expect('Content-Type', /json/)
         .end(function (err, res) {
           if (err) return done(err);
-          expect(res.body.name).to.be.eql('Lost World');
+          expect(res.body.name).to.be.eql(newWorldName);
           done();
         });
-
     })
   })
-
 });
