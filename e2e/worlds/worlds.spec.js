8fa09cc8f9476b24dac62685c9906f428e0505dd
jgartee <garteejohn@johndeere.com>
2016-02-29 11:49:10 -0500

    smoke test for multiple worlds on database

--- e2e/worlds/worlds.spec.js
+++ e2e/worlds/worlds.spec.js
@@ -34,28 +34,79 @@ describe('the application opens to the Worlds screen by default and ...', functi
       expect(element(by.id('addWorldButton')).isPresent()).toBe(true);
     });
 
-    it('displays a newly-added world as a hyperlink after entering a new world and clicking the add button',function(){
+    it('displays a newly-added world as a hyperlink after entering a new world and clicking the add button',function(done){
+
       var inputName=element(by.id('worldNameInput'));
+      var addButton=element(by.id('addWorldButton'));
+      var ec = protractor.ExpectedConditions;
+
       inputName.sendKeys('Pandora');
+      addButton.click();
+
+      var worldList = element(by.id('worldsList'));
+      browser.wait(ec.presenceOf(worldList),1000).then(function(){
+        var worldLink=worldList.all(by.id('worldLink')).first();
+
+        expect(worldLink.getText()).toContain('Pandora');
+      }, function(err){
+        fail();
+      }).then(done);
     });
 
-    it('displays multiple newly added worlds in alphebetical order',function(){
-
-    });
   });
 
   describe('when there are existing worlds in the database and...',function() {
     describe('when the application is started at the default routing',function() {
-
-      it('displays the name of all of the worlds on file as hyperlinks', function () {
-
+      beforeEach(function(done){
+        setup.purgeData()
+          .then(setup.addWorld('Pandora'))
+          .then(setup.addWorld('Pillar'))
+          .then(browser.get('/'))
+          .then(done);
       });
 
-      it('displays the world names on file in alphabetical order', function () {
-        //don't have yet
+      afterEach(function(done){
+        setup.purgeData()
+          .then(done);
+      });
+
+      it('displays the name of all of the worlds on file as hyperlinks', function (done) {
+
+        var ec = protractor.ExpectedConditions;
+        var worldList = element(by.id('worldsList'));
+        browser.wait(ec.presenceOf(worldList),1000).then(function(){
+          var worldList=element.all(by.id('worldsList'));
+
+          expect(worldList.getText()).toEqual(['Pandora','Pillar']);
+        }, function(){
+          fail();
+        }).then(done);
       });
 
       it('displays an edit icon after each entry', function () {
+
+      });
+    });
+
+    describe('when adding a new world to a set of existing worlds',function(){
+
+      beforeEach(function(done){
+        setup.purgeData()
+          .then(setup.addWorld('Pandora', 'Pillar'))
+          .then(browser.get('/'))
+          .then(done);
+      });
+
+      afterEach(function(done){
+        setup.purgeData()
+          .then(done);
+      });
+
+      it('adds a new world to the list after entering the text and the add button is clicked',function(){
+
+      });
+
+      it('displays the new world with italic and bold attributes', function() {
 
       });
     });
@@ -91,19 +142,5 @@ describe('the application opens to the Worlds screen by default and ...', functi
       });
     });
 
-    describe('when adding a new world to a set of existing worlds',function(){
-
-      it('adds a new world to the list after entering the text and the add button is clicked',function(){
-
-      });
-
-      it('adds a new world in alphabetical order',function(){
-        //need to add this functionality
-      });
-
-      it('displays the new world with italic and bold attributes', function() {
-
-      });
-    });
   });
 });
--- server/components/dataService.js
+++ server/components/dataService.js
@@ -1,14 +1,11 @@
 var mongoose = require("mongoose");
-
 var config = require('../config/environment');
-// /components/dataService.js
-// /config/environment/index.js
 
 module.exports = {
 	connect: function() {
 			 mongoose.connection.on('error', function(err) {
-				 // console.log("connection already opened");
 			 });
+
 			 mongoose.connect(config.mongo.uri);
 		 }
 }
