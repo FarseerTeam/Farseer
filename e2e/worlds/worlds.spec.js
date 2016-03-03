0efa7d44a039a75a59e405c4bdaf339715732b48
dimpar <dmitry.paremski@gmail.com>
2016-03-03 17:22:26 -0500

    adding more selenium tests for available icons in the world screen

--- e2e/worlds/worlds.spec.js
+++ e2e/worlds/worlds.spec.js
@@ -157,9 +157,9 @@ describe('the application opens to the Worlds screen by default and ...', functi
       });
 
       it('displays a save, an undo, and a delete icon after the text box when the edit icon is clicked',function(done) {
-        checkIfIconIsDisplayed(done, '.fa-floppy-o');
-        checkIfIconIsDisplayed(done, '.fa-undo');
-        checkIfIconIsDisplayed(done, '.fa-trash');
+        checkIfHtmlElementIsDisplayed(done, '.fa-floppy-o');
+        checkIfHtmlElementIsDisplayed(done, '.fa-undo');
+        checkIfHtmlElementIsDisplayed(done, '.fa-trash');
       });
 
       it('changes the hyperlink to a pre-filled text box when the edit icon is clicked', function(done){
@@ -172,31 +172,71 @@ describe('the application opens to the Worlds screen by default and ...', functi
           fail();
         }).then(done);
 
-        checkIfIconIsDisplayed(done, '#worldsList input');
-        checkIfIconIsNotDisplayed(done, '#worldsList a');
+        checkIfHtmlElementIsDisplayed(done, '#worldsList input');
+        checkIfHtmlElementIsNotDisplayed(done, '#worldsList a');
       });
 
       it('changes the text box to a hyperlink containing the new world name when the save icon is clicked', function(){
+        ////given
+        ////var textBoxes = element.all(by.model('updatedWorldName'));
+        ////var hyperlinks = element.all(by.css('#worldsList a'));
+        //var saveIcon = element(by.css('.fa-floppy-o'));
+        ////$httpBackend.whenPUT('/api/worlds/').respond(edgeCaseData);
+        ////when
+        //saveIcon.click().then(function() {
+        //  console.log("click ok!!");
+        //  checkIfHtmlElementIsDisplayed(done, '#worldsList a');
+        //  checkIfHtmlElementIsNotDisplayed(done, '#worldsList input');
+        //}, function(err) {
+        //  console.log("error", err);
+        //}).then(done);
+        //
+        ////then
 
       });
 
-      it('resets to the original world name after altering the text box and clicking the undo icon', function(){
+      it('resets to the original world name after altering the text box and clicking the undo icon', function(done) {
+        var newWorld = "Neptune1234";
+        var inputName=element(by.id('worldNameInput'));
+        var textBoxes = element.all(by.model('updatedWorldName'));
+        var hyperlinks = element.all(by.css('#worldsList a'));
 
+        inputName.sendKeys(newWorld);
+        var undoIcon = element(by.css('.fa-undo'));
+        undoIcon.click();
+
+        var worldList = element(by.id('worldsList'));
+        var worldListArr = element.all(by.id('worldsList'));
+        browser.wait(ec.presenceOf(worldList),1000).then(function(){
+          expect(worldListArr.count()).toEqual(2);
+          expect(worldListArr.getText()).toEqual(hyperlinks.getAttribute('innerHTML'));
+          expect(worldListArr.getText()).toEqual(textBoxes.getAttribute('value'));
+
+        }, function(err){
+          fail();
+        }).then(done);
+
+        checkIfHtmlElementIsDisplayed(done, '#worldsList a');
+        checkIfHtmlElementIsDisplayed(done, '.fa-pencil-square-o');
+        checkIfHtmlElementIsNotDisplayed(done, '#worldsList input');
+        checkIfHtmlElementIsNotDisplayed(done, '.fa-floppy-o');
+        checkIfHtmlElementIsNotDisplayed(done, '.fa-undo');
+        checkIfHtmlElementIsNotDisplayed(done, '.fa-trash');
       });
 
       it('deletes the world when the delete icon is clicked', function(){
 
       });
 
-      function checkIfIconIsDisplayed(done, cssSelector){
-        browser.wait(ec.presenceOf(worldList),1000).then(function(){
+      function checkIfHtmlElementIsDisplayed(done, cssSelector){
+        browser.wait(ec.presenceOf(worldList), 1000).then(function(){
           expect(element(by.css(cssSelector)).isDisplayed()).toBeTruthy();
         }, function(err){
           fail();
         }).then(done);
       }
 
-      function checkIfIconIsNotDisplayed(done, cssSelector){
+      function checkIfHtmlElementIsNotDisplayed(done, cssSelector){
         browser.wait(ec.presenceOf(worldList),1000).then(function(){
           expect(element(by.css(cssSelector)).isDisplayed()).toBeFalsy();
         }, function(err){
