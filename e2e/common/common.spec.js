21efca0eb7e8b308f13b0ff3fc5918ceb89b670c
Vera Reynolds <vera.reynolds29@gmail.com>
2016-04-08 12:06:15 -0600

    added sign in link to worlds page

--- e2e/common/common.spec.js
+++ e2e/common/common.spec.js
@@ -1,18 +1,25 @@
 'use strict';
 
 describe('Google sign in link', function() {
-  
-  it('appears on the players map page', function() {
-	  browser.get('/worlds/world/playersMap');
-    var signInLink = element(by.css('a#google-sign-in')); 
-    expect(signInLink.isPresent()).toBe(true);
-    expect(signInLink.getAttribute('href')).toBe('http://localhost:9001/auth/google');
-  }); 
-  
-  it('appears on the players roster page', function() {
-	  browser.get('/worlds/world/players');
-    var signInLink = element(by.css('a#google-sign-in')); 
-    expect(signInLink.isPresent()).toBe(true);
-    expect(signInLink.getAttribute('href')).toBe('http://localhost:9001/auth/google');
-  });
+
+    it('appears on the players map page', function() {
+        browser.get('/worlds/world/playersMap');
+        var signInLink = element(by.css('a#google-sign-in'));
+        expect(signInLink.isPresent()).toBe(true);
+        expect(signInLink.getAttribute('href')).toBe('http://localhost:9001/auth/google');
+    });
+
+    it('appears on the players roster page', function() {
+        browser.get('/worlds/world/players');
+        var signInLink = element(by.css('a#google-sign-in'));
+        expect(signInLink.isPresent()).toBe(true);
+        expect(signInLink.getAttribute('href')).toBe('http://localhost:9001/auth/google');
+    });
+
+    it('appears on the worlds page', function() {
+        browser.get('/worlds');
+        var signInLink = element(by.css('a#google-sign-in'));
+        expect(signInLink.isPresent()).toBe(true);
+        expect(signInLink.getAttribute('href')).toBe('http://localhost:9001/auth/google');
+    });
 });
