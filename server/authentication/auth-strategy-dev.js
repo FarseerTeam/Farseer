b449014307173bc8dcd945a5b2c95d6eba91f436
Vera Reynolds <vera.reynolds29@gmail.com>
2016-04-08 17:22:45 -0600

    updating the mongoose option to return the new item in callback instead of the old one

--- server/authentication/auth-strategy-dev.js
+++ server/authentication/auth-strategy-dev.js
@@ -4,7 +4,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var users = require('../components/users');
 
 var _verifyCB = function(username, password, done) {
-    users.User.findOneAndUpdate({ email: username }, { email: username }, { upsert: true }, function(err, user) {
+    users.User.findOneAndUpdate({ email: username }, { email: username }, { upsert: true, new: true }, function(err, user) {
         if (err) {
             console.log(err);
         }
--- server/authentication/google-auth.js
+++ server/authentication/google-auth.js
@@ -4,22 +4,22 @@ var config = require('../config/environment');
 var users = require('../components/users');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-var _verifyCB = function (accessToken, refreshToken, profile, done) {
-  users.User.findOneAndUpdate({ email: profile.emails[0].value }, { email: profile.emails[0].value }, { upsert: true }, function (err, user) {
-    if (err) {
-      console.log(err);
-    }
-    return done(err, user);
-  });
+var _verifyCB = function(accessToken, refreshToken, profile, done) {
+    users.User.findOneAndUpdate({ email: profile.emails[0].value }, { email: profile.emails[0].value }, { upsert: true, new: true }, function(err, user) {
+        if (err) {
+            console.log(err);
+        }
+        return done(err, user);
+    });
 };
 
 var _strategy = new GoogleStrategy({
-  clientID: config.googleClientId,
-  clientSecret: config.googleClientSecret,
-  callbackURL: config.googleAuthCallbackURL,
-  scope: 'https://www.googleapis.com/auth/plus.login email'
+    clientID: config.googleClientId,
+    clientSecret: config.googleClientSecret,
+    callbackURL: config.googleAuthCallbackURL,
+    scope: 'https://www.googleapis.com/auth/plus.login email'
 }, _verifyCB);
 
 module.exports = {
-  strategy: _strategy
+    strategy: _strategy
 };
