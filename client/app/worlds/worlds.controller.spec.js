6c0b80b87c4649f751d266879cdae7db379a1b83
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-07-30 15:11:26 -0400

    FARSEER-37 new worlds will have to be unique names

--- client/app/worlds/worlds.controller.spec.js
+++ client/app/worlds/worlds.controller.spec.js
@@ -5,10 +5,10 @@ describe('Controller: WorldsCtrl', function () {
   beforeEach(module('farseerApp'));
 
   var WorldsCtrl,
-      scope,
-      mockService,
-      addedWorld,
-      testWorlds;
+    scope,
+    mockService,
+    addedWorld,
+    testWorlds;
 
 
   beforeEach(inject(function ($controller, $rootScope, $q) {
@@ -22,39 +22,39 @@ describe('Controller: WorldsCtrl', function () {
       addWorld: function (newWorld) {
         addedWorld = newWorld;
         var deferred = $q.defer();
-        deferred.resolve({ data: newWorld });
+        deferred.resolve({data: newWorld});
         return deferred.promise;
       },
       updateWorld: function (worldId, updatedWorld) {
         var deferred = $q.defer();
-        deferred.resolve({ data: { id: updatedWorld + '-id', name: updatedWorld } });
+        deferred.resolve({data: {id: updatedWorld + '-id', name: updatedWorld}});
         return deferred.promise;
       },
       deleteWorld: function () {
         var deferred = $q.defer();
-        deferred.resolve({data: { ok: 1 }});
+        deferred.resolve({data: {ok: 1}});
         return deferred.promise;
       }
     };
 
     scope = $rootScope.$new();
     testWorlds = [
-        {name: 'Hogwarts School of Witchcraft and Wizardry', id: 'hogwartsschoolofwitchcraftandwizardry'}, 
-        {name: 'Beauxbatons Academy of Magic', id: 'beauxbatonsacademyofmagic'}, 
-        {name: 'Durmstrang Institute', id: 'durmstranginstitute'}, 
-        {name: 'Pillar', id: 'pillar'}];
+      {name: 'Hogwarts School of Witchcraft and Wizardry', id: 'hogwartsschoolofwitchcraftandwizardry'},
+      {name: 'Beauxbatons Academy of Magic', id: 'beauxbatonsacademyofmagic'},
+      {name: 'Durmstrang Institute', id: 'durmstranginstitute'},
+      {name: 'Pillar', id: 'pillar'}];
     WorldsCtrl = $controller('WorldsCtrl', {
       $scope: scope,
       httpService: mockService
     });
   }));
 
-  describe('updating an existing world', function() {
+  describe('updating an existing world', function () {
 
-    it('should return an updated world from the service', function() {
+    it('should return an updated world from the service', function () {
       var SECOND_WORLD_INDEX = 1;
       var UPDATED_WORLD = 'Pandora';
-      
+
       scope.updateWorld(testWorlds[SECOND_WORLD_INDEX].id, testWorlds[SECOND_WORLD_INDEX].name, UPDATED_WORLD);
       scope.$digest();
 
@@ -62,7 +62,7 @@ describe('Controller: WorldsCtrl', function () {
       expect(scope.worlds[SECOND_WORLD_INDEX].id).toBe(UPDATED_WORLD + '-id');
     });
 
-    it('should not update if world not found', function() {
+    it('should not update if world not found', function () {
       var SECOND_WORLD_INDEX = 1;
       var NON_EXISTING_WORLD = 'asdf';
       var UPDATED_WORLD = 'Pandora';
@@ -72,7 +72,7 @@ describe('Controller: WorldsCtrl', function () {
       expect(scope.worlds[SECOND_WORLD_INDEX].name).not.toBe(UPDATED_WORLD);
     });
 
-    it('should show alert and set updatedWorldName to old world name if http request is rejected', function(){
+    it('should show alert and set updatedWorldName to old world name if http request is rejected', function () {
       var expectedMessage = 'The Displayed Message';
       var SECOND_WORLD_INDEX = 1;
       var worldId = testWorlds[SECOND_WORLD_INDEX].id;
@@ -89,7 +89,7 @@ describe('Controller: WorldsCtrl', function () {
       expect(scope.updatedWorldNames[SECOND_WORLD_INDEX]).toBe(worldName);
     });
 
-    function setupMockServiceToRejectUpdateWorldAndReturnMessage(messageToReturn){
+    function setupMockServiceToRejectUpdateWorldAndReturnMessage(messageToReturn) {
       inject(function ($controller, $rootScope, $q) {
         mockService = {
           getWorlds: function () {
@@ -97,7 +97,7 @@ describe('Controller: WorldsCtrl', function () {
             deferred.resolve(testWorlds);
             return deferred.promise;
           },
-          updateWorld : function () {
+          updateWorld: function () {
             var deferred = $q.defer();
             deferred.reject({data: {message: messageToReturn}});
             return deferred.promise;
@@ -132,7 +132,7 @@ describe('Controller: WorldsCtrl', function () {
     var newWorld;
 
     beforeEach(function () {
-      newWorld = { id: 'Warcraft' };
+      newWorld = {id: 'Warcraft'};
       scope.newWorld = newWorld;
     });
 
@@ -162,7 +162,7 @@ describe('Controller: WorldsCtrl', function () {
       expect(_.last(scope.worlds)).toBe(newWorld);
     });
 
-    it('should show alert with message and set newWorld to empty string if http request is rejected', function(){
+    it('should show alert with message and set newWorld to empty string if http request is rejected', function () {
       var expectedMessage = 'Here is a Message';
       scope.newWorld = 'I am a new world and I am happy to be added';
 
@@ -180,7 +180,17 @@ describe('Controller: WorldsCtrl', function () {
       expect(window.alert).toHaveBeenCalledWith(expectedMessage);
     });
 
-    function setupMockServiceToRejectAddWorldAndReturnMessage(messageToReturn){
+    it('should not add a duplicate world to worlds in scope', function () {
+      addedWorld = undefined;
+      scope.newWorld = {name: 'Hogwarts School of Witchcraft and Wizardry', id: 'hogwartsschoolofwitchcraftandwizardry'}
+
+      scope.$digest();
+      scope.addWorld();
+
+      expect(addedWorld).toBe(undefined);
+    });
+
+    function setupMockServiceToRejectAddWorldAndReturnMessage(messageToReturn) {
       inject(function ($controller, $rootScope, $q) {
         mockService = {
           getWorlds: function () {
@@ -188,7 +198,7 @@ describe('Controller: WorldsCtrl', function () {
             deferred.resolve(testWorlds);
             return deferred.promise;
           },
-          addWorld : function () {
+          addWorld: function () {
             var deferred = $q.defer();
             deferred.reject({data: {message: messageToReturn}});
             return deferred.promise;
@@ -204,14 +214,14 @@ describe('Controller: WorldsCtrl', function () {
     }
   });
 
-  describe('delete a world', function() {
+  describe('delete a world', function () {
     var world;
 
     beforeEach(function () {
-      world = { id: 'Warcraft' };
+      world = {id: 'Warcraft'};
     });
 
-    it('should delete a world on confirmation', function() {
+    it('should delete a world on confirmation', function () {
       var EXPECTED_DELETE_WORLD_STATUS = 1;
       spyOn(window, 'confirm').and.returnValue(true);
 
@@ -221,7 +231,7 @@ describe('Controller: WorldsCtrl', function () {
       expect(scope.deleteWorldStatus).toBe(EXPECTED_DELETE_WORLD_STATUS);
     });
 
-    it('should not delete a world on cancellation', function() {
+    it('should not delete a world on cancellation', function () {
       spyOn(window, 'confirm').and.returnValue(false);
 
       scope.deleteWorldOnUserConfirmation(world);
